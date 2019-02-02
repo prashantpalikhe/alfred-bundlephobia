bbf811e6ed3aeab6409c9e1db5b278692ad9fc71
Prashant Palikhe <prashpal09@gmail.com>
2019-02-02 09:47:33 +0100

    fix: add missing JS interpolation for version

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ async function main() {
 	try {
 		const { name, version, gzip, size } = await getPackageStats(alfy.input);
 
-		alfy.output([{
-            title: `${ name}@${ version }`,
-            subtitle: `Size: ${formatBytes(size)}, Gzip: ${formatBytes(gzip)}`,
-	    arg: `https://bundlephobia.com/result?p=${name}@{version}`,
-	    quicklookurl: `https://bundlephobia.com/result?p=${name}@{version}`
-        }]);
+		alfy.output([
+			{
+				title: `${name}@${version}`,
+				subtitle: `Size: ${formatBytes(size)}, Gzip: ${formatBytes(
+					gzip
+				)}`,
+				arg: `https://bundlephobia.com/result?p=${name}@${version}`,
+				quicklookurl: `https://bundlephobia.com/result?p=${name}@${version}`
+			}
+		]);
 	} catch (e) {
 		alfy.error(e.message);
 	}
